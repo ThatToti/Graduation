96a14e38e8ff53b398b32cc33156715558f97d5f
Toti <totihuang@163.com>
2016-12-15 22:20:45 +0800

    更新进度

--- index.js
+++ index.js
@@ -36,6 +36,20 @@ app.use(require('express-formidable'))({
 	keepExtensions:true//保留后缀
 });
 
+// 设置模板全局常量
+app.locals.blog = {
+  title: pkg.name,
+  description: pkg.description
+};
+
+// 添加模板必需的三个变量
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user;
+  res.locals.success = req.flash('success').toString();
+  res.locals.error = req.flash('error').toString();
+  next();
+});
+
 routes(app);
 //路由
 
